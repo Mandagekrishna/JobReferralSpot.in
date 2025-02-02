8d7205f73fcafc59d5c924b7db24cf6417f6fc8e
krishna mandge <krishna.s.mandge@gmail.com>
2025-02-02 19:40:14 +0530

    vercel refresh error

--- src/Components/HOME/Footer.jsx
+++ src/Components/HOME/Footer.jsx
@@ -2,44 +2,28 @@ import styles from '../../Styles/FooterStyles.module.css'
 import {NavLink} from 'react-router-dom'
 
 
+import Grid from '@mui/material/Grid2';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { Card, Paper } from '@mui/material';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/material';
+
+
+
 export default function Footer()
 {
     return(
         <>
-            <div className={styles.container}>
-
-
-
-{//uncomment below section for accepting Donation
-            }
-
-                {/* <div className={styles.donationContainer}>
-                    <p>Your donation keeps me motivated to run and <br/>
-                    keep sharing on this site—thank you for your support!!!"</p>
-
-                    <p>
-                        you can make donation by buying thank you card from below
-                    </p>
-
-                    <button type='button' className={styles.button}>Donate</button>
-                </div> */}
-
-                
-
-
-                <div className={styles.footercontainer}>
-                  
-                <NavLink to='/Internships'><button type='button' className={styles.footercontainer} >Internships</button><br/></NavLink>
-                <NavLink to='/FreshersJobsList'><button type='button' className={styles.footercontainer}>Freshers Jobs</button><br/></NavLink>
-                <NavLink to='/ExperiencedJobs'><button type='button' className={styles.footercontainer}>Experienced Jobs</button><br/></NavLink>
-                <NavLink to='/WalkInDriveJobsList'><button type='button' className={styles.footercontainer}>Walk-IN Drives</button><br/></NavLink>
-                
-                </div>
-
-
-            </div>
-        
-        
-        </>
+    
+        <Grid container sx={{marginTop:10}} >
+            <Grid size={{md:12}}  sx={{backgroundColor:'#263238'}}>
+                    <NavLink to='/Internships'><button type='button' className={styles.footercontainer} >Internships</button><br/></NavLink>
+                    <NavLink to='/FreshersJobsList'><button type='button' className={styles.footercontainer}>Freshers Jobs</button><br/></NavLink>
+                    <NavLink to='/ExperiencedJobs'><button type='button' className={styles.footercontainer}>Experienced Jobs</button><br/></NavLink>
+                    <NavLink to='/WalkInDriveJobsList'><button type='button' className={styles.footercontainer}>Walk-IN Drives</button><br/></NavLink>
+              </Grid>
+        </Grid>     
+    </>
     )
 }
